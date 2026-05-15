dade72eb0d398f74970c0e7223619e57cb6ebf03
elina-dkn <elinadadakhanova@gmail.com>
2026-05-15 13:40:00 -0400

    split exerxise display

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ async function renderSplits() {
     const div = document.createElement("div");
 
     div.className = "card";
-    div.addEventListener("click", () => openSplit(ex.id));
+    div.addEventListener("click", () => openSplit(ex.id, ex.name));
 
     div.innerHTML = `
     <b>${ex.name}</b>
@@ -137,14 +137,25 @@ async function renderSplits() {
   `;
 
 
-    //div.querySelector("button").onclick = () =>
-    //    deleteDoc(doc(db, "users", user.uid, "splits", ex.id)) && renderSplits();
+    div.querySelector("button").onclick = () =>
+        deleteDoc(doc(db, "users", user.uid, "splits", ex.id)) && renderSplits();
 
     list.appendChild(div);
   }
 }
 
-async function openSplit(exID) {
+async function openSplit(exID, exName) {
+  const dialog = document.getElementById("splitDialog");
+  dialog.innerHTML = "";
+  const div = document.createElement("dialog");
+  div.className = "dialog";
+  const p = document.createElement("h3");
+  p.textContent = exName;
+  div.appendChild(p);
+  dialog.appendChild(div);
+
+
+
   const itemsSnap = await getDocs(
         collection(
             db,
@@ -163,15 +174,9 @@ async function openSplit(exID) {
 
   if(items.length > 0) {
     for (const i of items) {
-      const exSnap = await getDoc(
-          doc(
-              db,
-              "users",
-              user.uid,
-              "exercises",
-              i.exerciseId,
-          )
-      );
+      const exRef = doc(db, "users", user.uid, "exercises", i.exerciseId);
+      const exSnap = await getDoc(exRef);
+
       if (!exSnap.exists()) {
         console.log("Exercise not found");
 
@@ -190,14 +195,81 @@ async function openSplit(exID) {
         continue;
       }
 
+      const logsSnap = await getDocs(collection(exRef, "logs"));
+      const logs = logsSnap.docs.map(doc => doc.data());
+
+      logs.sort(
+          (a, b) => new Date(b.date) - new Date(a.date)
+      );
+
+      const latestWeight =
+          logs.length > 0
+              ? logs[0].weight
+              : "-";
+
       const exercise = exSnap.data();
       console.log(exercise.name);
+      const divContainer = document.createElement("div");
 
+      divContainer.className = "card";
+
+      divContainer.innerHTML = `
+    <b>${exercise.name}</b>
+
+    <div class="latest-weight">
+      Last: ${latestWeight} kg
+    </div>
+
+    <input type="number" placeholder="Weight">
+
+    <button class="delete-btn">
+      Delete from split
+    </button>
+  `;
+
+
+      const input = divContainer.querySelector("input");
+
+      input.onchange = async () => {
+        const logsRef = collection(exRef, "logs");
+
+        await addDoc(logsRef, {
+          date: new Date().toISOString(),
+          weight: Number(input.value)
+        });
+      };
+
+      divContainer.querySelector("button").onclick = async () =>
+          await deleteDoc(
+              doc(
+                  db,
+                  "users",
+                  user.uid,
+                  "splits",
+                  exID,
+                  "items",
+                  i.itemId
+              )
+          );
+
+      div.appendChild(divContainer);
     }
+
+    const closeButton = document.createElement("button");
+    closeButton.innerHTML = "Close";
+    closeButton.id = "close";
+    closeButton.className = "button";
+    div.appendChild(closeButton);
+
+    closeButton.addEventListener("click", () => div.close());
+    div.showModal();
   }
 
 
 }
+
+
+
 
 window.addExercise = async function () {
   const inputName = document.getElementById("newExercise");
@@ -258,7 +330,7 @@ window.addExercise = async function () {
 
   inputName.value = "";
   inputWeight.value = "";
-  split = "";
+  //split = "";
   await render();
 };
 
